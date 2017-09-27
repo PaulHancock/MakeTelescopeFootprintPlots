744e69ecc8308475100329fdef039ffc122ea7c6
PaulHancock <paul.hancock@curtin.edu.au>
2017-09-27 16:24:46 +0800

    create healpix version of earth map

---
+++ EarthHPX.py
@@ -0,0 +1,24 @@
+#! python
+from PIL import Image
+import healpy as hp
+from matplotlib.image import pil_to_array
+import numpy as np
+
+__author__ = 'Paul Hancock'
+__date__ = ''
+
+
+if __name__ == '__main__':
+    grayscale_pil_image = Image.open("shadedrelief.jpg").convert("L")
+    image_array = pil_to_array(grayscale_pil_image)
+    theta = np.linspace(0, np.pi, num=image_array.shape[0])[:, None]
+    phi = np.linspace(-np.pi, np.pi, num=image_array.shape[1])
+
+    nside = 512
+    print "Pixel area: %.2f square degrees" % hp.nside2pixarea(nside, degrees=True)
+
+    pix = hp.ang2pix(nside, theta, phi)
+    healpix_map = np.zeros(hp.nside2npix(nside), dtype=np.double)
+    healpix_map[pix] = image_array
+
+    hp.write_map('earth.fits', healpix_map, coord='C')
--- Makefile
+++ Makefile
@@ -6,7 +6,10 @@ shadedrelief.jpg:
 	wget https://github.com/matplotlib/basemap/raw/master/lib/mpl_toolkits/basemap/data/shadedrelief.jpg
 
 
-fits: AAT.fits MLO.fits CTIO.fits
+earth.fits: shadedrelief.jpg
+	python EarthHPX.py
+
+fits: AAT.fits MLO.fits CTIO.fits MWA.fits SALT.fits
 
 %.fits: telescopes.dat
 	grep $(firstword $(subst ., ,$@)) telescopes.dat | tr "," ' ' | awk '{print "MIMAS --fitsimage -o "$$1".fits +c "$$2" "$$3" " 90-$$4}' | bash
