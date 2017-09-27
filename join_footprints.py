2e9283645e9d99efa5c4fc9d85da8736dcf1017e
PaulHancock <paul.hancock@curtin.edu.au>
2017-09-27 16:44:04 +0800

    scale probability to FoV/available sky

--- join_footprints.py
+++ join_footprints.py
@@ -1,6 +1,7 @@
 #! python
 from __future__ import print_function
 from astropy.io import fits
+import numpy as np
 
 __author__ = 'Paul Hancock'
 __date__ = '26/09/2017'
@@ -8,17 +9,27 @@ __date__ = '26/09/2017'
 
 if __name__ == "__main__":
     files = []
+    fovs = []
+    elims = []
     with open('telescopes.dat') as f:
         for l in f.readlines():
             if l.startswith('#'):
                 continue
-            files.append(l.split()[0][:-1]+'.fits')  # telescope name is first col, and has a trailing ','
+            line = l.split('#')[0]  # strip comments
+            files.append(line.split(',')[0]+'.fits')
+            elims.append(float(line.split(',')[3]))
+            fovs.append(float(line.split(',')[4]))
+
+
     print(files[0])
     hdu = fits.open(files[0])
-    for f in files[1:]:
-        print('+'+f)
-        hdu2 = fits.open(f)
-        for i, row in enumerate(hdu2[1].data):
-            hdu[1].data[i][0] += row[0]
+    for i in range(1, len(files)):
+        print('+'+files[i])
+        hdu2 = fits.open(files[i])
+        # sky_area = 2*np.pi*(1-np.cos(np.radians(90-elims[i])))
+        # view_area = 2*np.pi*(1-np.cos(np.radians(fovs[i])))
+        fac = (1-np.cos(np.radians(90-elims[i]))) / (1-np.cos(np.radians(fovs[i])))
+        for j, row in enumerate(hdu2[1].data):
+            hdu[1].data[j][0] += row[0] * fac
     print('-> sum.fits')
     hdu.writeto('sum.fits', overwrite=True)
